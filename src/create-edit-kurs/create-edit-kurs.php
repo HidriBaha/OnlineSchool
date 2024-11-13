60dffc8d095e22229514e6c3eabee23d9f0a9b69
fo6636s <fabian.oswald@alumni.fh-aachen.de>
2024-11-13 16:25:54 +0100

    merge #4 bearbeiten von Kursen Schueler

--- src/create-edit-kurs/create-edit-kurs.php
+++ src/create-edit-kurs/create-edit-kurs.php
@@ -13,7 +13,7 @@ include "kurs.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css"
           rel="stylesheet">
-    <link rel="stylesheet" href="style.css"> <!-- Link to style.css -->
+    <link rel="stylesheet" href="../style.css"> <!-- Link to style.css -->
     <title></title>
 </head>
 <body>
@@ -22,7 +22,7 @@ include "kurs.php";
         <div class="container">
             <!-- School Logo on the left -->
             <a class="navbar-brand" href="#">
-                <img src="img/school-logo.png" class="logo" alt="School Logo">
+                <img src="../img/school-logo.png" class="logo" alt="School Logo">
             </a>
 
             <!-- Navbar Links (Aligned to Left) -->
---
+++ src/edit-kurs-schueler/CoursesOverview.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php
+    $courseList = [
+        'course1' => [
+            'name' => 'Course Name 1',
+            'img-src' => 'images/SWE Kursverwaltung.png',
+	        'course-link' => 'Teacher-EditCourse.php',
+        ],
+        'course2' => [
+            'name' => 'Course Name 2',
+            'img-src' => '',
+            'course-link' => ''
+        ]
+    ];
+?>
+
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <title>Courses</title>
+        <link rel="stylesheet" href="CoursesOverview.css">
+    </head>
+    <body>
+        <header>
+	        HEADER PLACEHOLDER
+        </header>
+        <div class="main">
+            <?php foreach ($courseList as $courseID => $courseInfos) {
+                echo "<a href='".$courseInfos['course-link']."'>
+				<div class='course-display'>
+                    <div class='course-desc'>".$courseInfos['name']."</div>
+                    <div class='course-img'><img alt='COURSE IMG' src='".$courseInfos['img-src']."'></div>
+                </div></a>";
+            }?>
+        </div>
+    </body>
+</html>
