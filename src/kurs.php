4fb7290aa21056ef20dd2b7116a8222e4141dc56
fo6636s <fabian.oswald@alumni.fh-aachen.de>
2024-11-16 13:27:05 +0100

    #9 umstyling Create/Edit-Kurs

--- src/kurs.php
+++ src/kurs.php
@@ -1,44 +1,138 @@
 <?php
 global $kurs;
-$kurs = [
-    "author" => "user",
-    "beschreibung" => "das ist ein Kurs",
-    "kapitel" => [
-        [
-            "definition" => "Das ist ein Definition",
-            "erklaerungen" => [
-                [
-                    "header"=> "Erklärung die Zweite",
-                    "erklaerung" => "Berechen sie die Nst",
-                    "img-src" => ""
+$id = $_GET["kursID"] ?? 0;
+$kurse = [
+    [
+        "titel"=> "Geometrie I",
+        "author" => "user",
+        "beschreibung" => "Das ist der erste Kurs",
+        "kapitel" => [
+            [
+                "definition" => "Das ist ein Definition",
+                "erklaerungen" => [
+                    [
+                        "header" => "Erklärung die Zweite",
+                        "erklaerung" => "Berechen sie die Nst",
+                        "img-src" => ""
+                    ]
+                ],
+                "aufgaben" => [
+                    [
+                        "aufgabenstellung" => "welche schnittpunkte hat die Gerade",
+                        "tipp" => "Nst berechen",
+                        "img-src" => "",
+                        "loesungen" => [0, 1]
+                    ]
                 ]
             ],
-            "aufgaben" => [
-                [
-                    "aufgabenstellung" => "welche schnittpunkte hat die Gerade",
-                    "tipp" => "Nst berechen",
-                    "img-src" => "",
-                    "loesungen" => [0, 1]
-                ]
-            ]
-        ],
-        [
-            "definition" => "Noch eine Definition",
-            "erklaerungen" => [
-                [
-                    "header"=> "Erklärung die Zweite",
-                    "erklaerung" => "Berechen sie die Nst",
-                    "img-src" => ""
+            [
+                "definition" => "Noch eine Definition",
+                "erklaerungen" => [
+                    [
+                        "header" => "Erklärung die Zweite",
+                        "erklaerung" => "Berechen sie die Nst",
+                        "img-src" => ""
+                    ]
+                ],
+                "aufgaben" => [
+                    [
+                        "aufgabenstellung" => "welche schnittpunkte hat die Gerade",
+                        "tipp" => "Nst berechen",
+                        "img-src" => "",
+                        "loesungen" => [0, 1]
+                    ]
                 ]
             ],
-            "aufgaben" => [
-                [
-                    "aufgabenstellung" => "welche schnittpunkte hat die Gerade",
-                    "tipp" => "Nst berechen",
-                    "img-src" => "",
-                    "loesungen" => [0, 1]
-                ]
-            ]
         ],
+    ],
+    [
+        "titel"=> "Zahlenmenge",
+        "author" => "user",
+        "beschreibung" => "das ist ein Kurs",
+        "kapitel" => [
+            [
+                "definition" => "Das ist ein Definition",
+                "erklaerungen" => [
+                    [
+                        "header" => "Erklärung die Zweite",
+                        "erklaerung" => "Berechen sie die Nst",
+                        "img-src" => ""
+                    ]
+                ],
+                "aufgaben" => [
+                    [
+                        "aufgabenstellung" => "welche schnittpunkte hat die Gerade",
+                        "tipp" => "Nst berechen",
+                        "img-src" => "",
+                        "loesungen" => [0, 1]
+                    ]
+                ]
+            ],
+            [
+                "definition" => "Noch eine Definition",
+                "erklaerungen" => [
+                    [
+                        "header" => "Erklärung die Zweite",
+                        "erklaerung" => "Berechen sie die Nst",
+                        "img-src" => ""
+                    ]
+                ],
+                "aufgaben" => [
+                    [
+                        "aufgabenstellung" => "welche schnittpunkte hat die Gerade",
+                        "tipp" => "Nst berechen",
+                        "img-src" => "",
+                        "loesungen" => [0, 1]
+                    ]
+                ]
+            ],
+        ]
+    ],
+    [
+        "titel"=> "Rechengesetze",
+        "author" => "user",
+        "beschreibung" => "das ist ein Kurs",
+        "kapitel" => [
+            [
+                "definition" => "Das ist ein Definition",
+                "erklaerungen" => [
+                    [
+                        "header" => "Erklärung die Zweite",
+                        "erklaerung" => "Berechen sie die Nst",
+                        "img-src" => ""
+                    ]
+                ],
+                "aufgaben" => [
+                    [
+                        "aufgabenstellung" => "welche schnittpunkte hat die Gerade",
+                        "tipp" => "Nst berechen",
+                        "img-src" => "",
+                        "loesungen" => [0, 1]
+                    ]
+                ]
+            ],
+            [
+                "definition" => "Noch eine Definition",
+                "erklaerungen" => [
+                    [
+                        "header" => "Erklärung die Zweite",
+                        "erklaerung" => "Berechen sie die Nst",
+                        "img-src" => ""
+                    ]
+                ],
+                "aufgaben" => [
+                    [
+                        "aufgabenstellung" => "welche schnittpunkte hat die Gerade",
+                        "tipp" => "Nst berechen",
+                        "img-src" => "",
+                        "loesungen" => [0, 1]
+                    ]
+                ]
+            ],
+        ]
     ]
+
 ];
+
+
+$kurs = $kurse[$id];
