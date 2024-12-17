41cafb524890049ce4393ead3e7b697726b68728
fo6636s <fabian.oswald@alumni.fh-aachen.de>
2024-12-17 18:44:49 +0100

    bugfix

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,9 +1,8 @@
 -- Erstellen der Datenbank
-DROP DATABASE IF EXISTS SchulDB;
-CREATE DATABASE SchulDB;
+CREATE DATABASE IF NOT EXISTS SchulDB;
 
 -- 2. Tabelle für users-Informationen
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(50) NOT NULL UNIQUE,
     passwort VARCHAR(255) NOT NULL, -- Passwort sollte gehashed sein
@@ -13,3 +12,5 @@ CREATE TABLE users (
     geburtsdatum DATE NOT NULL,
     adresse VARCHAR(100) NOT NULL
 );
+
+INSERT INTO USERS(EMAIL, PASSWORT, ROLLE, VORNAME, NACHNAME, GEBURTSDATUM, ADRESSE) VALUE ('admin@mail.de','$2y$10$qMPOugy6pREJxH5d51mbMe62luoMC7iSXhy/kOXVl4jo90LoC.tmK','admin','admin','admin','2001-12-05','test');
